d0b33e30bc377010a13d657cfa01e1f62910bdf8
edaceltuk <eceltuk@gmail.com>
2020-07-01 19:04:08 +0300

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import WeekContainer from './WeekContainer';
-import Server from '../public/index'
 const App = () => {
 
   return (
     <div className="App">
-      <Server />
       <WeekContainer />
     </div>
   )
