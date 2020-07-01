62d437d415de6f57f5bad949bcdea72d61529cf7
edaceltuk <eceltuk@gmail.com>
2020-07-01 18:48:01 +0300

    deploy

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', './public/index.html'));
 });
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
